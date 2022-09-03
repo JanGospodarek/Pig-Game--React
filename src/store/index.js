358301ee4b410312dd05b5ec9637974a25651a4d
JanGospodarek <jan.gospo@onet.pl>
2022-09-03 11:34:54 +0200

    added store without logic

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+const initialGameState = {
+  players: [
+    {
+      id: 1,
+      score: 1,
+      currentScore: 0,
+    },
+    {
+      id: 2,
+      score: 2,
+      currentScore: 0,
+    },
+  ],
+  activePlayer: 0,
+  dice: 1,
+};
+const gameSlice = createSlice({
+  name: "counter",
+  initialState: initialGameState,
+  reducers: {},
+});
+
+const store = configureStore({
+  reducer: { game: gameSlice.reducer },
+});
+export const gameActions = gameSlice.actions;
+export default store;
